0203d8b210f5386b4438eaf75c66889309238b6e
ottoorling <ottoorling@gmail.com>
2020-02-03 13:05:06 +0200

    viesti

---
+++ app.js
@@ -0,0 +1,9 @@
+var header1 = document.createElement("h1");
+header1.innerHTML = "Otsikko"
+document.body.appendChild(header1);
+
+var button1 = document.createElement("button");
+button1.innerHTML = "nappula"
+document.body.appendChild(button1);
+
+document.getElementsByName("tausta").style.backgroundColor = "blue"
